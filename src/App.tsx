432114570bedcb7cb64b56dfd57670e3262efc23
elliehachemjob <elliehachemjob@gmail.com>
2021-11-25 14:20:51 +0200

    Adding Navigation With React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import SearchComponent from "./components/SearchComponent";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
